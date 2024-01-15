c55eb9bc6b4bf58bf17bfa55280f90267ffda888
saif-uddin-shrabon <saif06065@gmail.com>
2024-01-16 00:57:32 +0600

    Manage Permission

--- app/src/main/java/com/algostack/nir/view/frame/add.kt
+++ app/src/main/java/com/algostack/nir/view/frame/add.kt
@@ -1,16 +1,5 @@
 package com.algostack.nir.view.frame
 
-import android.net.Uri
-import android.os.Build
-import android.os.Bundle
-import android.view.LayoutInflater
-import android.view.View
-import android.view.ViewGroup
-import android.widget.Adapter
-import android.widget.AdapterView
-import android.widget.ArrayAdapter
-import android.widget.Toast
-import androidx.fragment.app.Fragment
 import android.Manifest
 import android.annotation.SuppressLint
 import android.app.Activity
@@ -19,25 +8,30 @@ import android.content.Intent
 import android.content.pm.PackageManager
 import android.graphics.Bitmap
 import android.graphics.ImageDecoder
+import android.net.Uri
+import android.os.Build
+import android.os.Bundle
+import android.os.Environment
 import android.provider.MediaStore
-import androidx.activity.result.contract.ActivityResultContracts
+import android.provider.Settings
+import android.view.LayoutInflater
+import android.view.View
+import android.view.ViewGroup
+import android.widget.AdapterView
+import android.widget.ArrayAdapter
+import android.widget.Toast
 import androidx.annotation.RequiresApi
 import androidx.core.app.ActivityCompat
 import androidx.core.content.ContextCompat
-import androidx.core.content.FileProvider
 import androidx.core.view.isVisible
+import androidx.fragment.app.Fragment
 import androidx.fragment.app.viewModels
-
-import com.algostack.nir.R
 import com.algostack.nir.databinding.FragmentAddBinding
 import com.algostack.nir.services.model.CreatData
 import com.algostack.nir.services.model.CreatePost
-import com.algostack.nir.services.model.PublicPostData
 import com.algostack.nir.utils.FileCompressor
-import com.algostack.nir.utils.ManagePermission
 import com.algostack.nir.utils.NetworkResult
 import com.algostack.nir.utils.TokenManager
-import com.algostack.nir.viewmodel.AuthViewModel
 import com.algostack.nir.viewmodel.PublicPostViewModel
 import dagger.hilt.android.AndroidEntryPoint
 import java.io.ByteArrayOutputStream
@@ -56,7 +50,8 @@ class add : Fragment() {
 
         private const val PERMISSION_REQUEST_CODE = 100
         private const val REQUEST_OPEN_GALLERY = 2
-        private const val REQUEST_TAKE_PHOTO = 1
+        private const val STORAGE_PERMISSION_CODE = 23
+
     }
 
 
@@ -72,7 +67,7 @@ class add : Fragment() {
     private lateinit var  fileCompressor: FileCompressor
     private lateinit var dialog: AlertDialog
     private var listImage: MutableList<File> = ArrayList()
-    private var selectedSelectImage: Int = 0
+    //private var selectedSelectImage: Int = 0
     private val listSelectImage = arrayOf("Take Photo", "Choose from Gallery")
 
     var selectedRentType = ""
@@ -117,12 +112,18 @@ class add : Fragment() {
         // photo picker
         binding.addphoto.setOnClickListener {
 
-            selectedSelectImage = 1
+          //  selectedSelectImage = 1
+           // startGalleryIntent()
 
             if(checkPermission()){
-                openGalleryForImage()
+              //  openGalleryForImage()
+
+                startGalleryIntent()
             }else{
                 requestPermission()
+                println("checkPermission: ${checkPermission()}")
+                Toast.makeText(requireContext(), "Permission Denied", Toast.LENGTH_SHORT).show()
+
             }
 
 
@@ -458,18 +459,18 @@ binding.regContinue.setOnClickListener {
     }
 
 
-    private fun takePhoto() {
-        val intent = Intent(MediaStore.ACTION_IMAGE_CAPTURE)
-        fileImage = createFile()
-        val uri = if(Build.VERSION.SDK_INT >= 24){
-            FileProvider.getUriForFile(requireContext(), "com.gunawan.multipleimages.fileprovider",
-                fileImage)
-        } else {
-            Uri.fromFile(fileImage)
-        }
-        intent.putExtra(MediaStore.EXTRA_OUTPUT, uri)
-        startActivityForResult(intent, REQUEST_TAKE_PHOTO)
-    }
+//    private fun takePhoto() {
+//        val intent = Intent(MediaStore.ACTION_IMAGE_CAPTURE)
+//        fileImage = createFile()
+//        val uri = if(Build.VERSION.SDK_INT >= 24){
+//            FileProvider.getUriForFile(requireContext(), "com.gunawan.multipleimages.fileprovider",
+//                fileImage)
+//        } else {
+//            Uri.fromFile(fileImage)
+//        }
+//        intent.putExtra(MediaStore.EXTRA_OUTPUT, uri)
+//        startActivityForResult(intent, REQUEST_TAKE_PHOTO)
+//    }
 
     @SuppressLint("SimpleDateFormat")
     @Throws(Exception::class)
@@ -481,32 +482,74 @@ binding.regContinue.setOnClickListener {
 
 
     // Permission for camera and gallery
-    private fun checkPermission() :  Boolean{
-         return  (
-                 ContextCompat.checkSelfPermission( requireContext(), Manifest.permission.READ_EXTERNAL_STORAGE) == PackageManager.PERMISSION_GRANTED
-                                            &&
-                 ContextCompat.checkSelfPermission( requireContext(), Manifest.permission.WRITE_EXTERNAL_STORAGE) == PackageManager.PERMISSION_GRANTED
-                         &&
-                         ContextCompat.checkSelfPermission( requireContext(), Manifest.permission.CAMERA) == PackageManager.PERMISSION_GRANTED
+    private fun checkPermission(): Boolean {
+//        val readPermission = ContextCompat.checkSelfPermission(
+//            requireContext(),
+//            Manifest.permission.READ_EXTERNAL_STORAGE
+//        ) == PackageManager.PERMISSION_GRANTED
+//
+//
+//        val writePermission = ContextCompat.checkSelfPermission(
+//            requireContext(),
+//            Manifest.permission.WRITE_EXTERNAL_STORAGE
+//        ) == PackageManager.PERMISSION_GRANTED
+//
+//
+//        return readPermission && writePermission
 
-                 )
+
+        return if (Build.VERSION.SDK_INT >= Build.VERSION_CODES.R) {
+            //Android is 11 (R) or above
+            Environment.isExternalStorageManager()
+        } else {
+            //Below android 11
+            val write =
+                ContextCompat.checkSelfPermission(requireContext(), Manifest.permission.WRITE_EXTERNAL_STORAGE)
+            val read =
+                ContextCompat.checkSelfPermission(requireContext(), Manifest.permission.READ_EXTERNAL_STORAGE)
+            read == PackageManager.PERMISSION_GRANTED && write == PackageManager.PERMISSION_GRANTED
+        }
     }
 
     private fun requestPermission() {
-        ActivityCompat.requestPermissions(requireActivity(), arrayOf(Manifest.permission.CAMERA, Manifest.permission.WRITE_EXTERNAL_STORAGE, Manifest.permission.READ_EXTERNAL_STORAGE), PERMISSION_REQUEST_CODE)
+//        ActivityCompat.requestPermissions(
+//            requireActivity(),
+//            arrayOf(
+//                Manifest.permission.WRITE_EXTERNAL_STORAGE,
+//                Manifest.permission.READ_EXTERNAL_STORAGE
+//            ),
+//            PERMISSION_REQUEST_CODE
+//        )
+
+        //Android is 11 (R) or above
+        //Android is 11 (R) or above
+        if (Build.VERSION.SDK_INT >= Build.VERSION_CODES.R) {
+            val manageStorageIntent = Intent(Settings.ACTION_MANAGE_APP_ALL_FILES_ACCESS_PERMISSION)
+            manageStorageIntent.data = Uri.parse("package:" + requireContext().packageName)
+            startActivity(manageStorageIntent)
+        } else {
+            //Below android 11
+            ActivityCompat.requestPermissions(
+                requireActivity(), arrayOf(
+                    Manifest.permission.WRITE_EXTERNAL_STORAGE,
+                    Manifest.permission.READ_EXTERNAL_STORAGE
+                ),
+                STORAGE_PERMISSION_CODE
+            )
+        }
     }
 
 
-    override fun onRequestPermissionsResult(requestCode: Int, permissions: Array<out String>, grantResults: IntArray) {
-        if (requestCode == PERMISSION_REQUEST_CODE) {
-            if ((grantResults.isNotEmpty() && grantResults[0] == PackageManager.PERMISSION_GRANTED)
-                && grantResults[1] == PackageManager.PERMISSION_GRANTED && grantResults[2] == PackageManager.PERMISSION_GRANTED) {
-                if (selectedSelectImage == 0) {
-                   // takePhoto()
-                } else {
-                    openGalleryForImage()
-                }
 
+    @Deprecated("Deprecated in Java")
+    override fun onRequestPermissionsResult(
+        requestCode: Int,
+        permissions: Array<out String>,
+        grantResults: IntArray
+    ) {
+        if (requestCode == PERMISSION_REQUEST_CODE) {
+            if (grantResults.isNotEmpty() && grantResults.all { it == PackageManager.PERMISSION_GRANTED }) {
+                startGalleryIntent()
             } else {
                 Toast.makeText(requireContext(), "Permission Denied", Toast.LENGTH_SHORT).show()
             }
@@ -514,7 +557,17 @@ binding.regContinue.setOnClickListener {
     }
 
 
-
+    private fun startGalleryIntent() {
+        val intent = Intent().apply {
+            type = "image/*"
+            putExtra(Intent.EXTRA_ALLOW_MULTIPLE, true)
+            action = Intent.ACTION_GET_CONTENT
+        }
+        startActivityForResult(
+            Intent.createChooser(intent, "Select Picture"),
+            REQUEST_OPEN_GALLERY
+        )
+    }
 private fun bitmapToFile(bitmap: Bitmap): File {
     return try {
         fileImage = createFile()
@@ -563,7 +616,7 @@ private fun bitmapToFile(bitmap: Bitmap): File {
                     }
 
                       val tempFile = fileCompressor.compressToFile(bitmapToFile(bitmap))
-                    listImage.add(bitmapToFile(bitmap!!),)
+                    listImage.add(bitmapToFile(bitmap!!))
 
                     when (i) {
                         0 -> binding.imagepicker1.setImageURI(imageUri)
